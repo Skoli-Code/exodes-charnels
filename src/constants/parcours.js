22ac82a9aee19cb17834d0f6d56f798071e85aa9
pbellon <bellon.pierre@gmail.com>
2017-10-18 11:57:22 +0200

    Add test storymap & small description for parcours

--- src/constants/parcours.js
+++ src/constants/parcours.js
@@ -1,17 +1,24 @@
 const parcours = [{
   slug: 'fang-chine',
-  storymap_url: '',
-  description:``,
+  storymap_url: 'https://uploads.knightlab.com/storymapjs/cac43a2e018617cd5c087a55cdf7a0f7/map-de-test/index.html',
+  description:`43 ans  
+  Arrivée en France le 23 octobre 2015 de **Chine**
+  `,
   title: 'Fang',
 },{
   slug: 'joy-nigeria',
-  storymap_url: '',
-  description:``,
+  storymap_url: 'https://uploads.knightlab.com/storymapjs/cac43a2e018617cd5c087a55cdf7a0f7/map-de-test/index.html',
+  description:`22 ans  
+  Arrivée en France le 12 Septembre 2016 du **Nigéria**
+  `,
   title: 'Joy',
 },{
   slug: 'milena-bulgarie',
-  storymap_url: '',
-  description:``,
+  storymap_url: 'https://uploads.knightlab.com/storymapjs/cac43a2e018617cd5c087a55cdf7a0f7/map-de-test/index.html',
+  description:`
+  30 ans  
+  Arrivée en France le 5 Janvier 2005 de **Bulgarie**
+  `,
   title: 'Milena',
 }];
 
