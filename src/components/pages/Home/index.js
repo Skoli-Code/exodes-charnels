08fecf10ad652797c2111c5fc18a50d194169f2c
pbellon <bellon.pierre@gmail.com>
2017-10-20 18:42:01 +0200

    Implement home

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -12,6 +12,15 @@ const Home = () => (
     className='cover'
     style={styles}
   >
+    <div className='home-text'>
+      <h1>Exode(s) Charnel(s)</h1>
+      <h2 className='subtitle'>De la migration à la prostitution</h2>
+      <p>
+        Une application proposée par l’Univers de Lyon dans le cadre des rencontres
+        <span className='logo-white'/>
+      </p>
+    </div>
+
     <div className='button-holder'>
       <ButtonLink to='introduction'>EXODES CHARNELS</ButtonLink>
     </div>
