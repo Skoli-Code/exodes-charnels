83b49cf279ae7fcb3175fe737365dbe05a3ab11e
Lucas Piessat <lucaspiessat@macbook-pro.home>
2017-10-23 16:19:46 +0200

    change button intro

--- src/components/pages/Introduction.js
+++ src/components/pages/Introduction.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-import ContentContainer from 'containers/Content'; 
+import ContentContainer from 'containers/Content';
 import content from 'content/intro.md';
 import Content from 'components/atoms/Content';
 import ButtonLink from 'components/molecules/ButtonLink';
 import Centered from 'components/atoms/Centered';
-import PageTemplate from 'components/templates/PageTemplate'; 
+import PageTemplate from 'components/templates/PageTemplate';
 
 const Introduction = () => (
   <PageTemplate>
     <ContentContainer>
       <Content source={content}/>
       <Centered>
-        <ButtonLink to='parcours'>Démarrer</ButtonLink>
+        <ButtonLink to='parcours'>Découvrir les parcours</ButtonLink>
       </Centered>
     </ContentContainer>
   </PageTemplate>
