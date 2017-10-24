a64798586d77ce41d1b0e4b62b845a28beec37d8
Lucas Piessat <lucaspiessat@macbook-pro.home>
2017-10-24 16:03:57 +0200

    change ordre des parcours

--- src/constants/parcours.js
+++ src/constants/parcours.js
@@ -1,12 +1,4 @@
 const parcours = [{
-  slug: 'fang-chine',
-  storymap_url: 'https://uploads.knightlab.com/storymapjs/ce8e8bcc8dd8ae3a7d1c2161b0e39696/parcours-chinois/index.html',
-  description:`43 ans </br>
-  Arrivée en France le 23&nbsp;octobre 2015 de **Chine**
-  `,
-  pane_image: '/images/fang.jpg',
-  title: 'Fang',
-},{
   pane_image: '/images/keletso-rabalao-384640.jpg',
   slug: 'blessing-nigeria',
   storymap_url: 'https://uploads.knightlab.com/storymapjs/ce8e8bcc8dd8ae3a7d1c2161b0e39696/prostitute/index.html',
@@ -15,6 +7,15 @@ const parcours = [{
   `,
   title: 'Blessing',
 },{
+  slug: 'fang-chine',
+  storymap_url: 'https://uploads.knightlab.com/storymapjs/ce8e8bcc8dd8ae3a7d1c2161b0e39696/parcours-chinois/index.html',
+  description:`43 ans </br>
+  Arrivée en France le 23&nbsp;octobre 2015 de **Chine**
+  `,
+  pane_image: '/images/fang.jpg',
+  title: 'Fang',
+},
+{
   slug: 'milena-bulgarie',
   pane_image: '/images/milena.jpg',
   storymap_url: 'https://uploads.knightlab.com/storymapjs/ce8e8bcc8dd8ae3a7d1c2161b0e39696/parcours-bulgare/index.html',
