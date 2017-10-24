8705a726f43ad94b8b43d8ea08ac69d9ff5e3ee0
Lucas Piessat <lucaspiessat@macbook-pro.home>
2017-10-24 11:55:42 +0200

    change name : Joy -> Blessing

--- src/constants/parcours.js
+++ src/constants/parcours.js
@@ -1,25 +1,25 @@
 const parcours = [{
   slug: 'fang-chine',
   storymap_url: 'https://uploads.knightlab.com/storymapjs/ce8e8bcc8dd8ae3a7d1c2161b0e39696/parcours-chinois/index.html',
-  description:`43 ans  
+  description:`43 ans
   Arrivée en France le 23 octobre 2015 de **Chine**
   `,
   pane_image: '/images/fang.jpg',
   title: 'Fang',
 },{
   pane_image: '/images/keletso-rabalao-384640.jpg',
-  slug: 'joy-nigeria',
+  slug: 'blessing-nigeria',
   storymap_url: 'https://uploads.knightlab.com/storymapjs/ce8e8bcc8dd8ae3a7d1c2161b0e39696/prostitute/index.html',
-  description:`22 ans  
+  description:`22 ans
   Arrivée en France le 12 Septembre 2016 du **Nigéria**
   `,
-  title: 'Joy',
+  title: 'Blessing',
 },{
   slug: 'milena-bulgarie',
   pane_image: '/images/milena.jpg',
   storymap_url: 'https://uploads.knightlab.com/storymapjs/ce8e8bcc8dd8ae3a7d1c2161b0e39696/parcours-bulgare/index.html',
   description:`
-  30 ans  
+  30 ans
   Arrivée en France le 5 Janvier 2005 de **Bulgarie**
   `,
   title: 'Milena',
