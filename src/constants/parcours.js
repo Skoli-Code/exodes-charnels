7b1bf72096b3fe81f94c3e8f1c04385f5f12c85d
Lucas Piessat <lucaspiessat@macbook-pro.home>
2017-10-25 09:41:21 +0200

    small graphic changes

--- src/constants/parcours.js
+++ src/constants/parcours.js
@@ -2,14 +2,14 @@ const parcours = [{
   pane_image: '/images/keletso-rabalao-384640.jpg',
   slug: 'blessing-nigeria',
   storymap_url: 'https://uploads.knightlab.com/storymapjs/ce8e8bcc8dd8ae3a7d1c2161b0e39696/prostitute/index.html',
-  description:`22 ans </br>
+  description:`22 ans&nbsp;|
   Arrivée en France le 12&nbsp;Septembre 2016 du **Nigéria**
   `,
   title: 'Blessing',
 },{
   slug: 'fang-chine',
   storymap_url: 'https://uploads.knightlab.com/storymapjs/ce8e8bcc8dd8ae3a7d1c2161b0e39696/parcours-chinois/index.html',
-  description:`43 ans </br>
+  description:`43 ans&nbsp;|
   Arrivée en France le 23&nbsp;octobre 2015 de **Chine**
   `,
   pane_image: '/images/fang.jpg',
@@ -20,7 +20,7 @@ const parcours = [{
   pane_image: '/images/milena.jpg',
   storymap_url: 'https://uploads.knightlab.com/storymapjs/ce8e8bcc8dd8ae3a7d1c2161b0e39696/parcours-bulgare/index.html',
   description:`
-  30 ans </br>
+  30 ans&nbsp;|
   Arrivée en France le 5&nbsp;Janvier 2005 de **Bulgarie**
   `,
   title: 'Milena',
