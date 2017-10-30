4729868638e97c0f07ebf6764c8d6fefe0b2e8e8
pbellon <bellon.pierre@gmail.com>
2017-10-30 11:52:48 +0100

    Remove console.log

--- src/components/atoms/Underline/index.js
+++ src/components/atoms/Underline/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import './styles.css';
 
 const Underline = ({ children, ...props }) => {
-  console.log('Underline', props);
   return (
   <span class="underline">{ children }</span>
 );
