3833ee8a3d1fbab3806f39a7b87723f400f53b8e
pbellon <bellon.pierre@gmail.com>
2017-11-06 12:12:07 +0100

    Change back the URLs

--- src/constants/social.js
+++ src/constants/social.js
@@ -11,7 +11,7 @@ const TWEET_VIA_ACCOUNT = '';
 const TWEET_PARAMS = {
   via: TWEET_VIA_ACCOUNT,
   text: TWEET_TEXT,
-  url: 'https://exodes.universite-lyon.fr',
+  url: 'https://exodes-charnels.universite-lyon.fr',
   hashtags: TWEET_HASHTAGS,
 };
 
@@ -39,4 +39,4 @@ export const getTwitterShareHREF = () => {
 export const FACEBOOK_APP_ID = '1666596323384667';
 export const FACEBOOK_SDK_VERSION = 'v2.10';
 
-export const SHARE_URL = 'https://exodes.universite-lyon.fr/';
+export const SHARE_URL = 'https://exodes-charnels.universite-lyon.fr/';
